3ac28cebb3d3df4192076a9c0af6f2b00bb56045
Jonathan Rueda <jonaneiro09@gmail.com>
2024-10-04 15:53:12 +0200

    all

---
+++ src/main/java/com/project/hundirlaflota/Controller.java
@@ -0,0 +1,4 @@
+package com.project.hundirlaflota;
+
+public class Controller {
+}
---
+++ src/main/java/com/project/hundirlaflota/Main.java
@@ -0,0 +1,14 @@
+package com.project.hundirlaflota;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Main {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
