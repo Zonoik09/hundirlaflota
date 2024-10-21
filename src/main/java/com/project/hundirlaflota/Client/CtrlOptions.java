52641e53843c9efd42dcec6ce112c483979b435d
Jonathan Rueda <jonaneiro09@gmail.com>
2024-10-21 17:08:58 +0200

    ctrl

--- src/main/java/com/project/hundirlaflota/Client/CtrlOptions.java
+++ src/main/java/com/project/hundirlaflota/Client/CtrlOptions.java
@@ -1,4 +1,38 @@
 package com.project.hundirlaflota.Client;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
 public class CtrlOptions {
+    @FXML
+    private TextField protocol;
+    @FXML
+    private TextField serverIP;
+    @FXML
+    private TextField port;
+
+    @FXML
+    private void setConfigProxmox(String txtProtocol,String txtServerip, String txtPort) {
+        protocol.setText(txtProtocol);
+        serverIP.setText(txtServerip);
+        port.setText(txtPort);
+        System.out.println("Ha cambiado la config");
+    }
+
+    @FXML
+    private void getConfigProxmox() {
+        String txtProtocol = protocol.getText();
+        String txtServerip = serverIP.getText();
+        String txtPort = port.getText();
+        setConfigProxmox(txtProtocol,txtServerip,txtPort);
+    }
+
+    
+    public void changeView(ActionEvent actionEvent) {
+        getConfigProxmox();
+        UtilsViews.setView("ViewPagePrincipal");
+    }
 }
--- src/main/java/com/project/hundirlaflota/Client/CtrlPagePrincipal.java
+++ src/main/java/com/project/hundirlaflota/Client/CtrlPagePrincipal.java
@@ -1,4 +1,13 @@
 package com.project.hundirlaflota.Client;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import jdk.jshell.execution.Util;
+
 public class CtrlPagePrincipal {
+    public Button Button;
+
+    public void changeView(ActionEvent actionEvent) {
+        UtilsViews.setView("ViewOptions");
+    }
 }
